c1499cbafcf7bdaca360463da3564ed514b8efe0
kchen27 <kvc5823@psu.edu>
2020-10-01 02:24:33 +0000

    a better one

--- main.py
+++ main.py
@@ -4,15 +4,37 @@
 #Collaborator:
 
 def is_palindrome1(s):
-  counter=0
-  revste=s[::1]
-  for i in range (0,len(s),1):
-    if revste[1]==s[1]:
-      counter+=1
-    else:
-      counter+=0
-  if counter == len(s):
-    return True
+  i = len(s) - 1
+  j = 0
+  while (j < i):
+    if(s[j] != s[i]):
+      func1 = False
+    i = i - 1
+    j = j + 1
+  return func1
+
+def is_palindrome2(s):
+  func2 = True
+
+  if(len(s) - 1 <= 0):
+    return func2
+  if(s[0] == s[len(s) - 1]):
+    changed = s[1:-1]
+    return(is_palindrome2(changed))
   else:
-    return False
-    
+    func2 = False
+    return func2
+
+def is_palindrome3(s):
+  return(s == s[::-1])
+
+def run():
+  s = input("Enter a string: ")
+  print(is_palindrome1(s))
+  print(is_palindrome2(s))
+  print(is_palindrome3(s))
+
+if __name__ == "__main__":
+  run()
+
+ 
